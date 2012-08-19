570702dcf4bcc555be61ba867c887edc85423455
JVerstry <jverstry@gmail.com>
2012-08-19 20:55:33 +0200

    Conditional Javadoc Generation Example

---
+++ Conditional-Javadoc-Generation/src/main/java/com/jverstry/Generation/ClassWithJavadoc.java
@@ -0,0 +1,35 @@
+
+package com.jverstry.Generation;
+
+/**
+ * This is a class with some Javadoc.
+ */
+public class ClassWithJavadoc {
+	
+	/**
+	 * This is some attribute Javadoc
+	 */
+	private int someValue = 0;
+	
+	/**
+	 * This is some constructor Javadoc.
+	 */
+	public ClassWithJavadoc() {
+		
+	}
+
+	/**
+	 * This is some getter Javadoc.
+	 */
+	public int getSomeValue() {
+		return someValue;
+	}
+
+	/**
+	 * This is some setter Javadoc.
+	 */
+	public void setSomeValue(int someValue) {
+		this.someValue = someValue;
+	}
+	
+}
