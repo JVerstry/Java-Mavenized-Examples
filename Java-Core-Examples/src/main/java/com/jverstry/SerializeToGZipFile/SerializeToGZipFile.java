89aa68599cba05a0d81a976ad2cb81650d027704
JVerstry <jverstry@gmail.com>
2012-11-30 14:01:57 +0100

    Small clean-up

--- Java-Core-Examples/src/main/java/com/jverstry/SerializeToGZipFile/SerializeToGZipFile.java
+++ Java-Core-Examples/src/main/java/com/jverstry/SerializeToGZipFile/SerializeToGZipFile.java
@@ -1,19 +1,15 @@
 
 package com.jverstry.SerializeToGZipFile;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.io.ObjectInput;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutput;
 import java.io.ObjectOutputStream;
-import java.io.OutputStreamWriter;
 import java.util.TreeSet;
 import java.util.zip.GZIPInputStream;
 import java.util.zip.GZIPOutputStream;
