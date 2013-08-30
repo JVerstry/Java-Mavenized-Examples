c08dd4edfbfb917402ef36ddd416672be1ca280d
JVerstry <jverstry@gmail.com>
2013-08-30 18:19:18 +0200

    Encoding & decoding query string example

--- Java-Core-Examples/src/main/java/com/jverstry/QueryStrings/EncodeDecodeQueryStringsExample.java
+++ Java-Core-Examples/src/main/java/com/jverstry/QueryStrings/EncodeDecodeQueryStringsExample.java
@@ -2,17 +2,21 @@
 package com.jverstry.QueryStrings;
 
 import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
 import java.net.URLEncoder;
 
-public class EncodeQueryStringsExample {
+public class EncodeDecodeQueryStringsExample {
     
     public static void main(String[] args) throws UnsupportedEncodingException {
      
         String paramValue = "with spaces and special &!=; characters";
+        
         String encoded = URLEncoder.encode(paramValue, "UTF-8");
+        String decoded = URLDecoder.decode(paramValue, "UTF-8");
         
         System.out.println(paramValue);
         System.out.println(encoded);
+        System.out.println(decoded);
         
     }
     
